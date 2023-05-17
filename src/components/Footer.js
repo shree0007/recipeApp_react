32934a3e8143bf1b33f5c23df065c1bb3ca22681
shree0007 <shree.bhusal07@gmail.com>
2023-05-17 17:28:07 +0300

    added social media icons and linked them

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import './Footer.css';
+
+
 
 function Footer() {
     return (
         <footer className="footer">
-
-            <p>&copy; 2023 MyWebsite. All rights reserved.</p>
-
-
+            <p>&copy; 2023 Shree's Recipe Website. All rights reserved.</p>
+            <div className='icons'>
+                <Link to="https://www.facebook.com/mrfoodienepal"><FaFacebook size={24} className='icon' /></Link>
+                <Link to='https://twitter.com/fdbloggers?lang=en'><FaTwitter size={24} className='icon' /></Link>
+                <Link to='https://www.instagram.com/foodgasm_helsinki/'><FaInstagram size={24} className='icon' /></Link>
+            </div>
         </footer>
     );
 }
